842757fc5b6f8b018797d7de9c01ec68a3306e0a
PolinaSuvorova <polina.tatara@gmail.com>
2022-10-10 20:13:31 +0300

    commit1

---
+++ src/Calculator.java
@@ -0,0 +1,42 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 0;
+        int result;
+        char sign = '%';
+        if (sign == '-') {
+            result = a - b;
+        } else if (sign == '+') {
+            result = a + b;
+        } else if (sign == '*') {
+            result = a * b;
+        } else if (sign == '%') {
+            if (b != 0) {
+                result = a % b;
+            } else {
+                System.out.println("Деление на 0 не предусмотрено, расчёт пропущен");
+                return;
+            }
+        } else if (sign == '/') {
+            if (b != 0) {
+                result = a % b;
+            } else {
+                System.out.println("Деление на 0 не предусмотрено, расчёт пропущен");
+                return;
+            }
+        } else if (sign == '^') {
+            result = 1;
+            if (b != 0) {
+                for (int i = 1; i <= b; i++) {
+                    result *= a;
+                }
+            }
+        } else {
+            System.out.println("Знак вычисления " + sign + " не предусмотрен");
+            return;
+        }
+        System.out.println("a " + sign + " b = " + a + " " + sign + " " + b + " = "
+                + result);
+    }
+}
+
