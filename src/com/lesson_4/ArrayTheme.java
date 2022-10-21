4b897a5c365c84568876589b300a0b6bc44df7f9
PolinaSuvorova <polina.tatara@gmail.com>
2022-10-21 19:53:22 +0300

    21102022 1

--- src/com/lesson_4/ArrayTheme.java
+++ src/com/lesson_4/ArrayTheme.java
@@ -23,10 +23,31 @@ public class ArrayTheme {
         for (int i = 1; i < numbersArray.length - 1; i++) {
             multiplicationNumber *= numbersArray[i];
             System.out.print(numbersArray[i] + (i != numbersArray.length - 2 ?
-                    " * " : " = " +  multiplicationNumber + "\n"));
+                    " * " : " = " + multiplicationNumber + "\n"));
         }
-        System.out.println( numbersArray[0] + " " + numbersArray[9] );
+        System.out.println(numbersArray[0] + " " + numbersArray[9]);
 
+        System.out.println("\n3. Удаление элементов массива\n");
+        double[] randomNumbers = new double[15];
+        for (int i = 0; i < randomNumbers.length; i++) {
+            int localNumber = (int) (Math.random() * 1000);
+            randomNumbers[i] = localNumber;
+            randomNumbers[i] = randomNumbers[i]  / 1000;
+        }
+        System.out.println("Исходный массив:");
+        printArray(randomNumbers);
+        double midleArrayNumber = randomNumbers[randomNumbers.length / 2 ];
+        int countNilableNumber = 0;
+        System.out.printf("Число из середины массива: %6.3f", midleArrayNumber);
+        System.out.println();
+        for (int i = 0; i < randomNumbers.length; i++) {
+            if ( randomNumbers[i] > midleArrayNumber ) {
+                randomNumbers[i] = 0;
+                countNilableNumber++;
+            }
+        }
+        printArray(randomNumbers);
+        System.out.println("Всего обнулённых значений: " + countNilableNumber );
 
     }
 
@@ -34,6 +55,15 @@ public class ArrayTheme {
         for (int number : numbersArray
         ) {
             System.out.print(number + " ");
+        }
+        System.out.println();
+    }
+    private static void printArray(double[] numbersArray) {
+        for (int i = 0; i < numbersArray.length; i++) {
+            if (i == numbersArray.length / 2 + 1 ) {
+                System.out.println();
+            }
+            System.out.printf("%6.3f", numbersArray[i]);
         }
         System.out.println();
     }
