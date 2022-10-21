ea11191515d0286d9378d72ef3bae58674219430
PolinaSuvorova <polina.tatara@gmail.com>
2022-10-21 19:06:38 +0300

    21102022 1

--- src/com/lesson_4/ArrayTheme.java
+++ src/com/lesson_4/ArrayTheme.java
@@ -5,9 +5,8 @@ public class ArrayTheme {
         System.out.println("\n1.Реверс значений массива");
         int[] numbersArray = {5, 4, 6, 3, 1, 2, 7};
         int indexElement = numbersArray.length - 1;
-
         printArray(numbersArray);
-        for (int i = 0; i < (numbersArray.length - 1) / 2; i++) {
+        for (int i = 0; i < (numbersArray.length) / 2; i++) {
             int localElement = numbersArray[i];
             numbersArray[i] = numbersArray[indexElement];
             numbersArray[indexElement] = localElement;
@@ -15,12 +14,27 @@ public class ArrayTheme {
         }
         printArray(numbersArray);
 
+        System.out.println("\n2. Вывод произведения элементов массива");
+        numbersArray = new int[10];
+        for (int i = 0; i < numbersArray.length; i++) {
+            numbersArray[i] = i;
+        }
+        int multiplicationNumber = 1;
+        for (int i = 1; i < numbersArray.length - 1; i++) {
+            multiplicationNumber *= numbersArray[i];
+            System.out.print(numbersArray[i] + (i != numbersArray.length - 2 ?
+                    " * " : " = " +  multiplicationNumber + "\n"));
+        }
+        System.out.println( numbersArray[0] + " " + numbersArray[9] );
+
 
     }
 
     private static void printArray(int[] numbersArray) {
-        for (int number : numbersArray) {
+        for (int number : numbersArray
+        ) {
             System.out.print(number + " ");
         }
+        System.out.println();
     }
 }
