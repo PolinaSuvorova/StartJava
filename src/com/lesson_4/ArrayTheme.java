7562dbbb8f0e8da60587188c08b82b9e5148f6a0
PolinaSuvorova <polina.tatara@gmail.com>
2022-10-21 21:35:26 +0300

    21102022 4

--- src/com/lesson_4/ArrayTheme.java
+++ src/com/lesson_4/ArrayTheme.java
@@ -1,5 +1,7 @@
 package com.lesson_4;
 
+import java.util.Arrays;
+
 public class ArrayTheme {
     public static void main(String[] args) {
         System.out.println("\n1.Реверс значений массива");
@@ -27,28 +29,89 @@ public class ArrayTheme {
         }
         System.out.println(numbersArray[0] + " " + numbersArray[9]);
 
-        System.out.println("\n3. Удаление элементов массива\n");
+        System.out.println("\n3. Удаление элементов массива");
         double[] randomNumbers = new double[15];
         for (int i = 0; i < randomNumbers.length; i++) {
-            int localNumber = (int) (Math.random() * 1000);
-            randomNumbers[i] = localNumber;
-            randomNumbers[i] = randomNumbers[i]  / 1000;
+            randomNumbers[i] = generateRandomNumber();
         }
         System.out.println("Исходный массив:");
         printArray(randomNumbers);
-        double midleArrayNumber = randomNumbers[randomNumbers.length / 2 ];
+        double midleArrayNumber = randomNumbers[randomNumbers.length / 2];
         int countNilableNumber = 0;
         System.out.printf("Число из середины массива: %6.3f", midleArrayNumber);
         System.out.println();
         for (int i = 0; i < randomNumbers.length; i++) {
-            if ( randomNumbers[i] > midleArrayNumber ) {
+            if (randomNumbers[i] > midleArrayNumber) {
                 randomNumbers[i] = 0;
                 countNilableNumber++;
             }
         }
         printArray(randomNumbers);
-        System.out.println("Всего обнулённых значений: " + countNilableNumber );
+        System.out.println("Всего обнулённых значений: " + countNilableNumber);
 
+        System.out.println("\n4. Вывод элементов массива лесенкой в обратном порядке");
+        char[] lettersArray = new char[26];
+        char letter = 'A';
+        for (int i = 0; i < lettersArray.length; i++) {
+            lettersArray[i] = letter;
+            letter++;
+        }
+        for (int i = lettersArray.length - 1; i >= 0; i--) {
+            for (int j = lettersArray.length - 1; j >= i; j--) {
+                System.out.print(lettersArray[j]);
+            }
+            System.out.println();
+        }
+
+        System.out.println("5. Генерация уникальных чисел");
+        numbersArray = new int[30];
+        for (int i = 0; i < numbersArray.length; i++) {
+            int randomNum;
+            do {
+                randomNum = generateRandomNumber(60, 100);
+            } while (!checkUniqueNumber(numbersArray, randomNum));
+            numbersArray[i] = randomNum;
+        }
+        Arrays.sort(numbersArray);
+        for (int i = 0; i < numbersArray.length; i++) {
+            if (i % 10 == 0) {
+                System.out.println();
+            }
+            System.out.printf("%4d", numbersArray[i]);
+        }
+
+        System.out.println("\n6. Сдвиг элементов массива");
+        String[] lines = {"    ", "AA", "", "BBB", "CC", "D", "    ", "E", "FF", "G", ""};
+        int countLinesNotNillable = 0;
+        for (String line : lines
+        ) {
+            if (!line.isBlank()) {
+                countLinesNotNillable++;
+            }
+        }
+        printArray(lines);
+
+        if (countLinesNotNillable != 0) {
+            String[] copyLines = new String[countLinesNotNillable];
+            countLinesNotNillable = 0;
+            int beginSourceIndex = 0;
+            int currentIndexCopyLines = 0;
+            for (int i = 0; i < lines.length; i++) {
+                if (!lines[i].isBlank()) {
+                    countLinesNotNillable++;
+                    if (beginSourceIndex == 0) {
+                        beginSourceIndex = i;
+                    }
+                } else if (countLinesNotNillable > 0) {
+                    System.arraycopy(lines, beginSourceIndex, copyLines, currentIndexCopyLines,
+                            countLinesNotNillable);
+                    currentIndexCopyLines += countLinesNotNillable;
+                    beginSourceIndex = 0;
+                    countLinesNotNillable = 0;
+                }
+            }
+            printArray(copyLines);
+        }
     }
 
     private static void printArray(int[] numbersArray) {
@@ -58,13 +121,45 @@ public class ArrayTheme {
         }
         System.out.println();
     }
+
     private static void printArray(double[] numbersArray) {
         for (int i = 0; i < numbersArray.length; i++) {
-            if (i == numbersArray.length / 2 + 1 ) {
+            if (i == numbersArray.length / 2 + 1) {
                 System.out.println();
             }
             System.out.printf("%6.3f", numbersArray[i]);
         }
         System.out.println();
     }
+
+    private static void printArray(String[] linesArray) {
+        System.out.printf("|");
+        for (int i = 0; i < linesArray.length; i++) {
+            System.out.printf("%4s|", linesArray[i]);
+        }
+
+        System.out.println();
+    }
+
+    private static double generateRandomNumber() {
+        int intNumber = (int) (Math.random() * 1000);
+        double doubleNumber = intNumber;
+        doubleNumber /= 1000;
+        return doubleNumber;
+    }
+
+    private static int generateRandomNumber(int min, int max) {
+        return (int) (min + (Math.random() * (max - min)));
+    }
+
+    private static boolean checkUniqueNumber(int[] numbersArray,
+                                             int checkNumber) {
+        for (int number : numbersArray) {
+            if (number == checkNumber) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
