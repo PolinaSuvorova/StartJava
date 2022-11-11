ed884d412076e0f7710e89fa10f1722ee2632703
PolinaSuvorova <polina.tatara@gmail.com>
2022-11-11 15:23:37 +0300

    10102022 5 ДЗ

---
+++ src/com/lesson5/queries.sql
@@ -0,0 +1,49 @@
+SELECT *
+  FROM jaegers;
+
+SELECT *
+  FROM jaegers
+ WHERE status = 'Active';
+
+SELECT *
+  FROM jaegers
+ WHERE mark = 'Mark-1'
+    OR mark = 'Mark-4';
+
+SELECT *
+  FROM jaegers
+ WHERE mark <> 'Mark-1'
+   AND mark <> 'Mark-4';
+
+SELECT *
+  FROM jaegers
+ORDER BY mark DESC;
+
+SELECT *
+  FROM jaegers
+ WHERE launch =
+       (SELECT MIN(launch)
+          FROM jaegers);
+
+SELECT *
+  FROM jaegers
+ WHERE kaijukill =
+       (SELECT MIN(kaijukill)
+       FROM jaegers);
+
+SELECT *
+  FROM jaegers
+ WHERE kaijukill =
+       (SELECT MAX(kaijukill)
+       FROM jaegers);
+
+SELECT AVG(weight)
+  FROM jaegers;
+
+UPDATE jaegers
+   SET kaijukill = kaijukill + 1
+ WHERE status = 'Active';
+
+DELETE
+  FROM jaegers
+ WHERE status = 'Destroyed';
